95a6c1e1a6b776e712c0f0ff3c020a1321faac06
Punit Kumar <50498807+punitkr03@users.noreply.github.com>
2023-09-27 03:21:54 +0000

    init

---
+++ blog-page/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+     colors: {
+      dark: "#1b1b1b",
+      light: "#fff",
+      accent: "7b00d3",
+      accentDark: "#ffdb4d",
+      gray: "#747474"
+     },
+     fontFamily: {
+       mr: ["var(--font-mr)"],
+       in: ["var(--font-in)"]
+     },
+    },
+  },
+  plugins: [],
+}
+export default config
